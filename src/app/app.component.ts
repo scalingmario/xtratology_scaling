d4c0270718e8ca6618977f25f06244bdfa59bfd7
Nova <mariofrodm@gmail.com>
2026-05-13 14:47:49 -0600

    build(ui): integracion tailwind css y topologia fsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'xtratology-web';
-}
+export class AppComponent {}
